10d7dfded84bcef8bd8fa1b588a4c8abe2d590fb
oa1cspace <oa1cspace@gmail.com>
2020-09-03 17:17:26 +0300

    Get ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CountState} from "./reducers/count/count.reducer";
+import {select, Store} from "@ngrx/store";
+import {Observable} from "rxjs";
+import {selectCount, selectUpdatedAt} from "./reducers/count/count.selectors";
+import {CountClearAction, CountDecreaseAction, CountIncreaseAction} from "./reducers/count/count.actions";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app-angular-tutorial';
+
+  public count$: Observable<number> = this.store$.pipe(select(selectCount));
+  public updatedAt$: Observable<number> = this.store$.pipe(select(selectUpdatedAt));
+
+  constructor(private store$: Store<CountState>) {
+  }
+
+  increase() {
+    this.store$.dispatch(new CountIncreaseAction())
+  }
+
+  decrease() {
+    this.store$.dispatch(new CountDecreaseAction())
+  }
+
+  clear() {
+    this.store$.dispatch(new CountClearAction())
+
+  }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+import {AppEffects} from "./app.effects";
+import {metaReducers, reducers} from "./reducers";
 
 @NgModule({
   declarations: [
@@ -10,7 +17,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
+    EffectsModule.forRoot([AppEffects]),
+    StoreRouterConnectingModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
